6de3f0ff0b16e2502dbf3b139cbb8ac3393faaca
tharsila <tharsilapaula@gmail.com>
2022-01-25 19:33:15 -0300

    add function initScrollSmooth e animationScroll

--- script.js
+++ script.js
@@ -49,3 +49,57 @@ function initAccordion () {
 
 initAccordion ()
 
+function initScrollSmooth () {
+
+  const navMenu = document.querySelectorAll('.js-menu a[href^="#"]');
+
+  function anchorActive (event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute('href');
+    const section = document.querySelector(href);
+
+    section.scrollIntoView({
+      behavior:'smooth',
+      block:'start',
+    })
+  }
+
+  navMenu.forEach((item) => {
+    item.addEventListener('click', anchorActive);
+  })
+}
+
+initScrollSmooth ()
+
+function initAnimationScroll () {
+
+  const sections = document.querySelectorAll('.js-scroll');
+
+  if (sections.length) {
+  
+    const windowHalf = window.innerHeight * 0.6;
+    
+    function scrollActive () {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const visibility = sectionTop -  windowHalf;
+    
+        if(visibility < 0) {
+          section.classList.add('active')
+        } else {
+          section.classList.remove('active')
+        } 
+        
+      })
+    }
+    
+    scrollActive ()
+    
+    window.addEventListener('scroll', scrollActive);
+  }
+
+}
+
+initAnimationScroll ()
+
+
