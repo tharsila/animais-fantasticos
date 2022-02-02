b4635b0e67b3c1cbe897d318a69f71dc543a07ae
tharsila <tharsilapaula@gmail.com>
2022-02-01 23:09:04 -0300

    add script module

---
+++ js/script.js
@@ -0,0 +1,9 @@
+import initScrollSmooth from './modules/scroll-smooth.js';
+import initScrollAnimation from './modules/scroll-animation.js';
+import initAccordion from './modules/accordion.js';
+import initTabNav from './modules/tabnav.js';
+
+initScrollSmooth ();
+initScrollAnimation ();
+initAccordion ();
+initTabNav ();
