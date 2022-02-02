ece79407694d19ad3dbb2908d980e18065d30c0d
tharsila <tharsilapaula@gmail.com>
2022-02-01 23:10:14 -0300

    add scroll animation

---
+++ js/modules/scroll-animation.js
@@ -0,0 +1,28 @@
+export default function initScrollAnimation () {
+
+  const sections = document.querySelectorAll('[data-anime="scroll"]');
+
+  if (sections.length) {
+  
+    const windowHalf = window.innerHeight * 0.6;
+    
+    function scrollActive () {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const visibility = sectionTop -  windowHalf;
+    
+        if(visibility < 0) {
+          section.classList.add('active')
+        } else {
+          section.classList.remove('active')
+        } 
+        
+      })
+    }
+    
+    scrollActive ()
+    
+    window.addEventListener('scroll', scrollActive);
+  }
+
+}
