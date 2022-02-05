24d6c1d5393dc86b445c253e6d1d702b99f8c584
tharsila <tharsilapaula@gmail.com>
2022-02-04 23:07:40 -0300

    add tooltip

---
+++ js/modules/tooltip.js
@@ -0,0 +1,44 @@
+export default function inittoolTip () {
+
+  const tooltip = document.querySelectorAll('[data-tooltip]');
+  
+  tooltip.forEach((item) => {
+    item.addEventListener('mouseover', onMouseOver);
+  })
+  
+  function onMouseOver () {
+    const tooltipBox = createTooltipBox(this);
+    
+    onMouseMove.tooltipBox = tooltipBox;
+    this.addEventListener('mousemove', onMouseMove)
+  
+    onMouseLeave.tooltipBox = tooltipBox;
+    onMouseLeave.element = this;
+    this.addEventListener('mouseleave', onMouseLeave)
+  }
+  
+  const onMouseLeave = {
+    handleEvent () {
+      this.tooltipBox.remove();
+      this.element.removeEventListener('mouseleave', onMouseLeave)
+      this.element.removeEventListener('mousemove', onMouseMove)
+    }
+  }
+  
+  const onMouseMove = {
+    handleEvent (event) {
+      this.tooltipBox.style.top = event.pageY + 20 +'px';
+      this.tooltipBox.style.left = event.pageX + 20 + 'px';
+    }
+  }
+  
+  function createTooltipBox (element) {
+    const tooltipBox = document.createElement('div');
+    const text = element.getAttribute('aria-label');
+    tooltipBox.classList.add('tooltip');
+    tooltipBox.innerText = text;
+    document.body.appendChild(tooltipBox);
+    return tooltipBox;
+  }
+  
+}
