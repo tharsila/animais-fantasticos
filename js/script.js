d7efb39a47ec5c518e2ddae6d367a6884f14fa7c
tharsila <tharsilapaula@gmail.com>
2022-02-04 23:06:52 -0300

    add initModal e initTooltip

--- js/script.js
+++ js/script.js
@@ -2,8 +2,12 @@ import initScrollSmooth from './modules/scroll-smooth.js';
 import initScrollAnimation from './modules/scroll-animation.js';
 import initAccordion from './modules/accordion.js';
 import initTabNav from './modules/tabnav.js';
+import initModal from './modules/modal.js';
+import initTooltip from './modules/tooltip.js'
 
 initScrollSmooth ();
 initScrollAnimation ();
 initAccordion ();
 initTabNav ();
+initModal ();
+initTooltip ();
