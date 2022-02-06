4659452209cf4de22be475bdda966f3f729ab567
tharsila <tharsilapaula@gmail.com>
2022-02-06 20:44:37 -0300

    add dropdown-menu

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@ import initScrollAnimation from './modules/scroll-animation.js';
 import initAccordion from './modules/accordion.js';
 import initTabNav from './modules/tabnav.js';
 import initModal from './modules/modal.js';
-import initTooltip from './modules/tooltip.js'
+import initTooltip from './modules/tooltip.js';
+import initDropdownMenu from './modules/dropdown-menu.js';
 
 initScrollSmooth ();
 initScrollAnimation ();
@@ -11,3 +12,4 @@ initAccordion ();
 initTabNav ();
 initModal ();
 initTooltip ();
+initDropdownMenu ();
