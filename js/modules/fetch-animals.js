672efa20a92c4125e0f60038ccaab5152103a94a
tharsila <tharsilapaula@gmail.com>
2022-02-16 21:33:54 -0300

    add function fetchAnimals e importa initNumbers

---
+++ js/modules/fetch-animals.js
@@ -0,0 +1,30 @@
+import initNumbers from './numbers.js';
+
+export default function initFetchAnimals () {
+  async function fetchAnimals (url) {
+    const animalsResponse = await fetch(url);
+    const animalsJSON = await animalsResponse.json();
+    const numberGrid = document.querySelector('.number-grid');
+  
+    animalsJSON.forEach((animal) => {
+      const divAnimal = createAnimal(animal);
+      numberGrid.appendChild(divAnimal);
+    });
+  
+    initNumbers();
+  }
+  
+  function createAnimal (animal) {
+    const div = document.createElement('div');
+    div.classList.add('number-animal');
+  
+    div.innerHTML = `
+      <h3>${animal.specie}</h3>
+      <span data-number>${animal.total}</span>
+    `
+    return div;
+  }
+  
+  fetchAnimals('./animalsapi.json')
+}
+
